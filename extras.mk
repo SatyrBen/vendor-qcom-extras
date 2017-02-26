8ec02b6aacd853c9d1388a481f02d3685dd16c16
A <A>
2017-02-26 10:07:29 +0100

    Add widevine to extras.mk

--- extras.mk
+++ extras.mk
@@ -3,3 +3,9 @@ ifneq ($(filter msm8974,$(TARGET_BOARD_PLATFORM)),)
   include vendor/qcom/extras/msm8974/graphics/libuiblur.mk
 
 endif
+
+ifneq ($(filter msm8996 msm8994 msm8992 msm8960 msm8952 msm8916 msm8974 msm8610,$(TARGET_BOARD_PLATFORM)),)
+
+  include vendor/qcom/extras/widevine/widevine-vendor.mk
+
+endif
